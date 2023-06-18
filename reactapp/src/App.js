01b53fcb9b8a49e63d2bf2d70054bf76d6068f9b
lakshmi2848 <lakshmiadusumalli24343@gmail.com>
2023-06-18 11:46:19 +0000

    Signup Page

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
-import Login from './components/Login';
-import './components/Login.css';
+import Login from './components/Auth/Login/Login';
+import './components/Auth/Login/Login';
+import './components/Auth/Signup/Signup';
+import Signup from './components/Auth/Signup/Signup';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/Signup" element={<Signup/>}/>
+        <Route path="/Login" element={<Login/>}/>
       </Routes>
     </Router>
   );
