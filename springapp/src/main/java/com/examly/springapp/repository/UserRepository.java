62f26f51cdd552a6580587961ba4997d40159b07
RagiriMaheshwari <maheshwar0711@gmail.com>
2023-06-28 11:03:16 +0000

    pom_repo_appPro

--- springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
@@ -4,14 +4,10 @@ import com.examly.springapp.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
+public interface UserRepo extends JpaRepository<UserModel, Integer> {
 
-
-public interface UserRepository extends JpaRepository<UserModel,Integer> {
-
-    UserModel save(UserModel userModel);
-    UserModel findById(Integer id);
-    void deleteById(Integer id);
-    
-    
+    Optional<UserModel> findByEmail(String email);
 }
