090d5d2a0c91cf69898b63db4cfe0a85a5c17920
RagiriMaheshwari <maheshwar0711@gmail.com>
2023-07-06 12:54:15 +0000

    updated UserModel

--- springapp/src/main/java/com/examly/springapp/model/UserModel.java
+++ springapp/src/main/java/com/examly/springapp/model/UserModel.java
@@ -43,52 +43,5 @@ public class UserModel {
         this.userRole = userRole;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public Set<String> getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(Set<String> userRole) {
-        this.userRole = userRole;
-    }
-
+    
 }
