944fed1c5557ee77b28727c42e842a17a2da8321
RagiriMaheshwari <maheshwar0711@gmail.com>
2023-07-06 05:52:47 +0000

    user

--- springapp/src/main/java/com/examly/springapp/model/UserModel.java
+++ springapp/src/main/java/com/examly/springapp/model/UserModel.java
@@ -20,13 +20,10 @@ public class UserModel {
     @Column(unique = true)
     private String email;
 
-    @Column(name ="password")
     private String password;
 
-    @Column(name ="username")
     private String username;
 
-    @Column(name ="mobileNumber")
     private String mobileNumber;
 
     @ElementCollection(fetch = FetchType.EAGER)
@@ -38,10 +35,60 @@ public class UserModel {
     private Set<String> userRole;
 
 
-    
+    public UserModel(String email, String password, String username, String mobileNumber, Set<String> userRole) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.mobileNumber = mobileNumber;
+        this.userRole = userRole;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Set<String> getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Set<String> userRole) {
+        this.userRole = userRole;
+    }
 
 }
