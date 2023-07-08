d61fc6167e1cbefdc38bd987f3b577488959ff58
Bishal Behera <bishalreigns390@gmail.com>
2023-07-08 20:28:31 +0530

    Added MyOrder Page and EditOrder Page

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
-import Login from './components/Login';
 import MyOrders from './components/MyOrders';
+import EditOrder from './components/EditOrder';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/Login" element={<Login />} />
         <Route path="/MyOrders" element={<MyOrders />} />
+        <Route path="/EditOrder" element={<EditOrder/>}/>
+        
       </Routes>
     </Router>
   );
--- reactapp/src/components/MyOrders.jsx
+++ reactapp/src/components/MyOrders.jsx
@@ -1,52 +1,85 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import classes from "./MyOrders.module.css";
+import classes from "./MyOrder.css";
+import { v4 as uuidv4 } from 'uuid';
+import EditOrder from "./EditOrder.js";
 import Header from "./Header";
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import  axios  from 'axios';
+
 
 
 const MyOrders = () => {
-  return (
-    <>
-      <Header />
-      <div className={classes["order-container"]}>
-        <h1>Gift-Name</h1>
-        <h1>Price</h1>
-        <h1>Quantity</h1>
-      </div>
 
-      <div className={classes["products"]}>
-        <ol>
-          <li>
-            <p className={classes["list-item-name"]}>Coffee</p>
-            <p  className={classes["list-item-quantity"]}>200</p>
-            <p className={classes["list-item-price"]}>3pcs</p>
-            <EditIcon/>
-            <DeleteIcon />
-          </li>
-          <li>
-            <p className={classes["list-item-name"]}>Coffee</p>
-            <p  className={classes["list-item-quantity"]}>200</p>
-            <p className={classes["list-item-price"]}>3pcs</p>
-            <EditIcon/>
-            <DeleteIcon />
-          </li>
-          <li>
-            <p className={classes["list-item-name"]}>Coffee</p>
-            <p  className={classes["list-item-quantity"]}>200</p>
-            <p className={classes["list-item-price"]}>3pcs</p>
-            <EditIcon/>
-            <DeleteIcon />
-          </li>
-        </ol>
-      </div>
+  const[CurrentUrl,setCurrentUrl]=useState("http://localhost:8081");
+  const data = [
+    { id: uuidv4(),name: "Photos", price: 19, quantity: 100 },
+    { id: uuidv4(),name: "Frbdfdgts", price: 319, quantity: 100 },
+    { id: uuidv4(),name: "Caards", price:25, quantity: 100 },
+    { id: uuidv4(),name: "Laptops Stickers", price:25, quantity: 100}
+  ];
 
-      <div className={classes["buttons-container"]}>
-        <button className={classes["button-arounder"]}>Pay</button>
-      </div>
-    </>
-  );
+  const navigate = useNavigate();
+  
+  const DeleteData = (index) => {
+    axios.delete(CurrentUrl+`/admin/deleteGift/${index}`)
+
+    .then((r) =>
+    {
+      console.log(r);
+    })
+    .catch((e) =>
+    {
+      console.log(e);
+    })
+  };
+    
+  const EditData = () => {
+    navigate("/EditOrder");
+  };
+
+return(
+  <>
+     <Header />
+    <div className='container-sm text-center'>
+   
+   <table class="table table-hover table-scripted table">
+      <thead class="table-info">
+        <tr class="order-containers">   
+          <th scope="col"><h3>GiftName</h3></th>
+          <th scope="col"><h3>Price</h3></th>
+          <th scope="col"><h3>Quantity</h3></th>
+        </tr>
+      </thead>
+  <tbody>
+ {data.map((items,index)=>
+     {
+       return(
+         <tr >
+           <td >{items.name}</td>
+           <td>{items.price}</td>
+           <td>{items.quantity}</td>
+
+           <div className='d-flex '>
+             <button className=' btn btn-outline'  onClick={()=>EditData()}><EditIcon/></button>
+             <button className=' btn btn-outline' onClick={()=>DeleteData(items.id)}><DeleteIcon /> </button>
+           </div>        
+         
+         </tr>
+       )
+     })}
+
+ </tbody>
+</table>
+ </div>
+
+       <div className="buttons-container">
+         <button className="button-arounder">Pay</button>
+       </div>
+  </>
+);
+
 };
-
+  
 export default MyOrders;
