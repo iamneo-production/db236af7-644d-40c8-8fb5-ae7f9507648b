7b40e6f46da4f9ba4e31b2d57f2dbe0d7d6a7197
VikramTaviti <vikramvikki314@gmail.com>
2023-07-15 15:49:58 +0000

    Adding LoginResponse

---
+++ springapp/src/main/java/com/examly/springapp/model/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.examly.springapp.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String role;
+
+}
