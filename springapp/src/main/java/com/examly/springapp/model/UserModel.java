c8ef4f2e6ed01614f3e1bcf37f738a0f859d8181
RagiriMaheshwari <maheshwar0711@gmail.com>
2023-07-15 18:11:43 +0000

    Usermodel

--- springapp/src/main/java/com/examly/springapp/model/UserModel.java
+++ springapp/src/main/java/com/examly/springapp/model/UserModel.java
@@ -16,8 +16,6 @@ public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private  int id;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
     @Column(unique = true)
     private String email;
@@ -35,39 +33,4 @@ public class UserModel {
     )
     @Column(name = "role")
     private Set<String> userRole;
-=======
-=======
->>>>>>> c7cc825e23703ef3c45000924527520f69b546a2
-
-    @Column(unique = true)
-    private String email;
-
-    private String password;
-
-    private String username;
-
-    private String mobileNumber;
-
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(
-            name = "roles_tab",
-            joinColumns = @JoinColumn(name = "id")
-    )
-    @Column(name = "role")
-    private Set<String> userRole;
-
-
-    public UserModel(String email, String password, String username, String mobileNumber, Set<String> userRole) {
-        this.email = email;
-        this.password = password;
-        this.username = username;
-        this.mobileNumber = mobileNumber;
-        this.userRole = userRole;
-    }
-
-    
-<<<<<<< HEAD
->>>>>>> 90c01362fb7715ea3f94b1c0262afff26bcf28c5
-=======
->>>>>>> c7cc825e23703ef3c45000924527520f69b546a2
 }
