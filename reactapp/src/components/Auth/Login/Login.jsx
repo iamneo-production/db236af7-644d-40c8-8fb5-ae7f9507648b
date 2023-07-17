71c9bcce191670c36ae5daa2be8db61beaff789e
Vishwajith1464 <n.m.vishwajith@gmail.com>
2023-07-17 12:47:25 +0000

    Login elements modified

--- reactapp/src/components/Auth/Login/Login.jsx
+++ reactapp/src/components/Auth/Login/Login.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.css';
+import axios from 'axios'
+import box from '../../../assets/box.png';
+import ribbon1 from '../../../assets/ribbon1.png';
+import ribbon2 from '../../../assets/ribbon2.png';
+import cornerBox from '../../../assets/corner-box.png'
+import mobileCornerBox from '../../../assets/mobile-corner-box.png'
+import logo from '../../../assets/giftlogo.svg'
 
 const Login = () => {
   const initialState = {
@@ -71,49 +78,31 @@ const Login = () => {
   const handleSignUp = () => {
     navigate("/signup")
   }
-
   return (
-    <div className="login-container">
-      <h1>Login</h1>
-      <form className="login-form" onSubmit={handleLogin}>
-        <div className="form-group">
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={state.email}
-            onChange={handleInputChange}
-            placeholder="Enter email"
-            className={errors.email ? 'error' : ''}
-          />
-          {errors.email && <div className="error-message">Email is required.</div>}
+    <div className="content-holder">
+      <img id="corner-box" src={cornerBox}></img>
+        <div className="content">
+          <img id="mobile-corner-box" src={mobileCornerBox}></img>
+             <div className = "mobile-logo">Customized&nbsp;Gifts&nbsp;<img src={logo}></img></div>
+                <div className = "logo">Customized<br></br><img src={logo}></img>&nbsp;Gifts</div>
+                <div className="LoginContainer">
+                    <img id="ribbon1" src={ribbon1}/>
+                    <img id="loginBox" src={box}/>
+                    <div className = "LoginForm">
+                        <p>Login</p>
+                        <input className="inputs"  type="email" onChange={handleInputChange} name="email" value={state.email} placeholder="Email address"></input>
+                        {errors.email && <div className="error-message">Invalid Email address!</div>}
+                        <input className="inputs" type="password" onChange={handleInputChange} name="password" value = {state.password} placeholder="Password"></input>
+                        { errors.password && <div className="error-message"> Password Required! </div>}
+                        { errors.custom.required && <div className="error-message">{ errors.custom.message } </div>}
+                        <button className="loginButton" onClick={handleLogin}>{loader ? <div className="loader"></div> : "Sign In"}</button>
+                        <span style={{cursor:"pointer"}} className="Signup" onClick={handleSignUp}> 
+                            New? Register Here
+                        </span>
+                    </div>
+                    <img id="ribbon2" src={ribbon2}/> 
+              </div>
         </div>
-
-        <div className="form-group">
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={state.password}
-            onChange={handleInputChange}
-            placeholder="Enter Password"
-            className={errors.password ? 'error' : ''}
-          />
-          {errors.password && (
-            <div className="error-message">
-              {errors.custom.required ? errors.custom.message : 'Password is required.'}
-            </div>
-          )}
-        </div>
-
-        <button  type="submit">Login</button>
-
-        <p className="new-user-message" id="signupLink">
-          New User/admin? <Link to="/signup">Sign up</Link>
-        </p>
-
-        {loader && <div className="loader"></div>}
-      </form>
     </div>
   );
 };
