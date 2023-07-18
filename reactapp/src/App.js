9abf52a18afd2b41e885f0c1b9ac65953b886786
shivx5 <sivakongunadu234@gmail.com>
2023-07-18 15:43:48 +0000

    routed App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login/Login";
 import Signup from "./components/Auth/Signup/Signup";
-import MyOrders from "./components/MyOrders";
+import MyOrders from "./components/MyOrders.jsx";
 import EditOrder from "./components/EditOrder";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -12,6 +12,11 @@ import Test from "./components/User/Test";
 import "./App.css";
 import AdminTest from "./components/Admin/AdminTest";
 import LoginRoute from "./components/routes/LoginRoute";
+import PlaceOrder from "./components/Customer/PlaceOrder/PlaceOrder";
+import HoamePage from './components/Customer/HoamePage/HoamePage';
+import Adminthemes from "./components/Admin/Adminthemes/Adminthemes";
+import AddGift from "./components/Admin/AddGift/AddGift";
+import Orders from "./components/Admin/Orders/Orders";
 
 function App() {
   axios.defaults.baseURL = "http://localhost:8080";
@@ -26,12 +31,17 @@ function App() {
           <Route element={<Login />} path="" />
         </Route>
         <Route element={<UserRoutes />} path="user">
-          <Route element={<Test />} path="test" />
+          <Route path="home"element={<HoamePage/> }/>
+          <Route path="test" element={<Test />}/>
           <Route path="myorders" element={<MyOrders />} />
           <Route path="editorder" element={<EditOrder />} />
+          <Route path="placeorder" element={<PlaceOrder/>}/>
         </Route>
         <Route element={<AdminRoutes />} path="admin">
           <Route element={<AdminTest />} path="gift" />
+          <Route element={<Adminthemes />} path="themes" />
+          <Route element={<AddGift />} path="gifts" />
+          <Route element={<Orders />} path="orders" />
         </Route>
         <Route path="/signup" element={<Signup />} />
       </Routes>
