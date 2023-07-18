459584e6aaf3ddfd016e085a436a0f2ed7b024a5
shivx5 <sivakongunadu234@gmail.com>
2023-07-18 15:37:45 +0000

    footer.js modified

--- reactapp/src/components/Customer/HoamePage/Footer.js
+++ reactapp/src/components/Customer/HoamePage/Footer.js
@@ -7,6 +7,8 @@ const Footer = () => {
     <>
       {/* <FooterLinks /> */}
       <Grid
+      position="absolute"
+      bottom="0"
         container
         spacing={0}
         sx={{
@@ -27,11 +29,11 @@ const Footer = () => {
           </Typography>
         </Grid>
         <Grid item xs={4}>
-          <img
+          {/* <img
             src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/payment-method_69e7ec.svg"
             className={classes["payment-methods-image"]}
             alt="payment-methods"
-          />
+          /> */}
         </Grid>
       </Grid>
     </>
