09412dfb98e156ffd8b318402275362b51155ca3
Sarthak Kumar Behera <sarthakbehera09@gmail.com>
2023-07-23 12:54:40 +0530

    Changes on imports

--- reactapp/src/components/Admin/AdminOrders/AdminViewOrders.jsx
+++ reactapp/src/components/Admin/AdminOrders/AdminViewOrders.jsx
@@ -1,67 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AdminHeader from "../AdminHeader/AdminHeader";
 import AdminViewOrdersTable from "./AdminViewOrdersTable";
+import axios from "axios";
 
 const AdminViewOrders = () => {
-//   const ORDERS_DUMMY_DATA = [
-//     {
-//       orderId: 1,
-//       orderEmail: "sarth@ss.com",
-//       giftName: "Frame",
-//       orderPrice: 23,
-//       orderAddress: "Street Colony, 448302",
-//     },
-//     {
-//       orderId: 2,
-//       orderEmail: "sarth@ss.com",
-//       giftName: "Frame",
-//       orderPrice: 23,
-//       orderAddress: "Street Colony, 448302",
-//     },
-//     {
-//       orderId: 3,
-//       orderEmail: "sarth@ss.com",
-//       giftName: "Frame",
-//       orderPrice: 23,
-//       orderAddress: "Street Colony, 448302",
-//     },
-//     {
-//       orderId: 4,
-//       orderEmail: "sarth@ss.com",
-//       giftName: "Frame",
-//       orderPrice: 23,
-//       orderAddress: "Street Colony, 448302",
-//     },
-//     {
-//       orderId: 5,
-//       orderEmail: "sarth@ss.com",
-//       giftName: "Frame",
-//       orderPrice: 23,
-//       orderAddress: "Street Colony, 448302",
-//     },
-//     {
-//       orderId: 6,
-//       orderEmail: "sarth@ss.com",
-//       giftName: "Frame",
-//       orderPrice: 23,
-//       orderAddress: "Street Colony, 448302",
-//     },
-//     {
-//       orderId: 7,
-//       orderEmail: "sarth@ss.com",
-//       giftName: "Frame",
-//       orderPrice: 23,
-//       orderAddress: "Street Colony, 448302",
-//     },
-//   ];
-
   const [ordersData, setOrdersData] = useState([]);
   useEffect(() => {
     axios.get("/admin/order").then((res) => {
-      Setallorder(res.data);
-       console.log(res)
+      setOrdersData(res.data);
     });
-  }, []);
+  }, [ordersData]);
 
   return (
     <>
