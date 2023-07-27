b92373f10a1dbdd4f17f3e46f362bf8c24fa1e8c
lakshmi2848 <lakshmiadusumalli24343@gmail.com>
2023-07-27 07:18:02 +0000

    added Url

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -31,7 +31,7 @@ import AdminTest from "./components/Admin/AdminTest";
 
 function App() {
   axios.defaults.baseURL =
-  "https://8080-ddaaedffdcbbfdebbecaddbaaecadafbad.project.examly.io";
+  "https://8080-dadecaeedcbbfdebbecaddbaaecadafbad.project.examly.io";
   const token = localStorage.getItem("Auth");
 
   if (token) axios.defaults.headers.common["Authorization"] = token;
